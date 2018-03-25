24ad77095f2620b3a9b68c02f0c3e86f70a551c3
HBalija <balija.hrvoje@gmail.com>
2018-03-25 14:28:42 +0200

    Update js code; jQuery version included

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,61 @@
-var element = document.querySelector('.warning')
-var image = document.querySelector('.image')
+// vanilla JS CODE
 
-element.style.display = 'none'
+var links = document.querySelectorAll('#imageGallery a')
 
-image.addEventListener('click', function () {
-  element.style.display === 'none' ? element.style.display = '' : element.style.display = 'none'
+var overlay = document.createElement('div')
+overlay.id = 'overlay'
+
+var image = document.createElement('img')
+var caption = document.createElement('p')
+
+overlay.appendChild(image)
+overlay.appendChild(caption)
+
+document.body.appendChild(overlay)
+
+links.forEach(function (link) {
+  link.addEventListener('click', function (event) {
+    event.preventDefault()
+
+    var imageLocation = this.attributes.href.textContent
+    image.setAttribute('src', imageLocation)
+
+    overlay.style.display = 'block'
+
+    var captionText = this.querySelector('img').alt
+    overlay.querySelector('p').textContent = captionText
+  })
 })
+
+overlay.addEventListener('click', function () {
+  this.style.display = 'none'
+})
+
+/*
+// jQuery code (jQuery inclusion commented in index.html)
+
+var $overlay = $('<div id="overlay"></div>')
+var $image = $('<img>')
+var $caption = $('<p></p>')
+
+$overlay.append($image)
+$overlay.append($caption)
+
+$('body').append($overlay)
+
+$('#imageGallery a').click(function (event) {
+  event.preventDefault()
+  var imageLocation = $(this).attr('href')
+  $image.attr('src', imageLocation)
+
+  $overlay.show()
+
+  var captionText = $(this).children('img').attr('alt')
+  $caption.text(captionText)
+})
+
+$overlay.click(function () {
+  $overlay.hide()
+})
+
+*/
